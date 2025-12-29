17efed3a31b0c36f531a4c062b98c7045ded4adc
lalitel <pity164@cancer-treatment.xyz>
2025-12-29 19:08:05 +0300

    transform notes

--- src/components/reader/ReaderView.tsx
+++ src/components/reader/ReaderView.tsx
@@ -1,15 +1,27 @@
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
 import './ReaderView.css';
 
 interface ReaderViewProps {
   content: string;
 }
 
+function transformFootnotes(text: string): string {
+  return text.replace(/\[\^(\d+)\]/g, '<sup class="footnote-ref">$1</sup>');
+}
+
 export default function ReaderView({ content }: ReaderViewProps) {
+  const processedContent = transformFootnotes(content);
+
   return (
     <article className="reader-view">
-      <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+      >
+        {processedContent}
+      </Markdown>
     </article>
   );
 }
